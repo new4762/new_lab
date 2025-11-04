8f59306cada95b340d567a5fd27b6ad1a36d0706
Kanisorn siripatkerdpong <kanisorn@omise.co>
2025-11-04 10:10:30 +0700

    try update

--- hoop/runbooks/demo.runbook.js
+++ hoop/runbooks/demo.runbook.js
@@ -13,10 +13,29 @@ node -e  "console.log('[DRYRUN] ' + {{ .note_msg
   | squote
 }})"
 
-node -e  "console.log({{ .refund_uids
+
+refund_uids = {{ .refund_uids
   | description "All refund uid to mark as void with separate by space"
   | placeholder "rfnd_1 rfnd_2 rfnd_3"
   | required "refund_uids is required"
   | type "text"
   | squote
-}}.split(" ");)"
+}}
+
+
+node - e "console.log(refund_uids)"
+
+// node -e  "console.log({{ .refund_uids
+//   | description "All refund uid to mark as void with separate by space"
+//   | placeholder "rfnd_1 rfnd_2 rfnd_3"
+//   | required "refund_uids is required"
+//   | type "text"
+//   | squote
+// }}.split(' ');) "
+
+// node -e "console.log('{{ .refund_uids
+//   | description \"All refund uid to mark as void separated by space\"
+//   | placeholder \"rfnd_1 rfnd_2 rfnd_3\"
+//   | required \"refund_uids is required\"
+//   | type \"text\"
+// }}'.split(' '))"
