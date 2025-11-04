dd51dbc795faac95a724b53f9a7b448487ec92c8
Kanisorn siripatkerdpong <kanisorn@omise.co>
2025-11-04 09:59:35 +0700

    try update js to test

--- hoop/runbooks/demo.runbook.js
+++ hoop/runbooks/demo.runbook.js
@@ -1,5 +1,22 @@
-node -e "console.log('{{ .output
-         | type "text"
-         | required "output is required"
-         | default "hello world"
+node -e  "console.log('{{ .user_email
+  | description "User email that will appear in the audit log"
+  | placeholder "@email.com"
+  | required "user_email is required"
+  | type "email"
+  | squote
 }}')"
+
+node -e  "console.log(''[DRYRUN] ' + {{ .note_msg
+  | type "text"
+  | required "note_msg is required"
+  | default "Customer requested refund to original payer"
+  | squote
+}}')"
+
+node -e  "console.log('{{ .refund_uids
+  | description "All refund uid to mark as void with separate by space"
+  | placeholder "rfnd_1 rfnd_2 rfnd_3"
+  | required "refund_uids is required"
+  | type "text"
+  | squote
+}}.split(" ");')"
